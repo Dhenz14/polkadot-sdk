567107509e09a293cd5a6d41d559dc6a79427cce
github-action-benchmark <github@users.noreply.github.com>
2026-03-11 15:22:44 +0000

    add notifications_protocol (cargo) benchmark result for 4ad511e82a5b9d7faf9ec10314aa011b566fd157

--- bench/notifications_protocol/data.js
+++ bench/notifications_protocol/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773236384939,
+  "lastUpdate": 1773242563691,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "notifications_protocol": [
@@ -116351,6 +116351,198 @@ window.BENCHMARK_DATA = {
             "name": "notifications_protocol/litep2p/with_backpressure/16MB",
             "value": 2291320065,
             "range": "± 52893415",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "14218860+iulianbarbu@users.noreply.github.com",
+            "name": "Iulian Barbu",
+            "username": "iulianbarbu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ad511e82a5b9d7faf9ec10314aa011b566fd157",
+          "message": "collator-protocol: check v3 candidate against last finished slot block (#11239)\n\n# Description\n\nThis PR achieves the following:\n1) removes the assumption that the scheduling parent sent with a\ncandidate descriptor is an active leaf on the collator-side\n2) adds a minor reputation penalty for collators advertisements with an\ninvalid scheduling parent, which ends up in a collator ban and\ndisconnect only when abuse happens (see #11339).\n3) checks on the validator_side that the candidate descriptor's\nscheduling parent is the rc block corresponding to the last finished rc\nslot's block.\n\n## Integration\n\nN/A\n\n## Review Notes\n\nFixes #11169 \n\nCumulus side here: https://github.com/paritytech/polkadot-sdk/pull/11241\nTests were done against the cumulus PR + the corresponding scheduling\nparent picking logic when building block (async backing with v2/v3\ncollations). Additional tests were made for the case of elastic scaling\ntoo (v2 & v3 alike). As a bonus, the changed logic removes the\n`slot_offset` for V3 collations building.\n\n---------\n\nSigned-off-by: Iulian Barbu <iulian.barbu@parity.io>\nCo-authored-by: eskimor <robert@gonimo.com>\nCo-authored-by: eskimor <eskimor@noreply.com>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>\nCo-authored-by: eskimor <jfanatiker@gmx.at>",
+          "timestamp": "2026-03-11T14:14:27Z",
+          "tree_id": "d9f2067a06303d9239f0f859e3aad6897918ef8f",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/4ad511e82a5b9d7faf9ec10314aa011b566fd157"
+        },
+        "date": 1773242538987,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "notifications_protocol/libp2p/serially/64B",
+            "value": 3935804,
+            "range": "± 21834",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/64B",
+            "value": 295319,
+            "range": "± 17962",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/512B",
+            "value": 3856767,
+            "range": "± 19188",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/512B",
+            "value": 376177,
+            "range": "± 4773",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/4KB",
+            "value": 4657892,
+            "range": "± 52634",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/4KB",
+            "value": 870003,
+            "range": "± 12044",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/64KB",
+            "value": 10065973,
+            "range": "± 53074",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/64KB",
+            "value": 4839120,
+            "range": "± 111813",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/256KB",
+            "value": 41439997,
+            "range": "± 1259686",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/256KB",
+            "value": 36013607,
+            "range": "± 631346",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/2MB",
+            "value": 329229254,
+            "range": "± 3536218",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/2MB",
+            "value": 277364946,
+            "range": "± 2311285",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/16MB",
+            "value": 2452092393,
+            "range": "± 21451113",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/16MB",
+            "value": 2274878090,
+            "range": "± 98371392",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/64B",
+            "value": 3031209,
+            "range": "± 15516",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/64B",
+            "value": 1548225,
+            "range": "± 38302",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/512B",
+            "value": 3161417,
+            "range": "± 16316",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/512B",
+            "value": 1606835,
+            "range": "± 8851",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/4KB",
+            "value": 3723243,
+            "range": "± 24794",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/4KB",
+            "value": 1931082,
+            "range": "± 14118",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/64KB",
+            "value": 7599342,
+            "range": "± 56754",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/64KB",
+            "value": 4884473,
+            "range": "± 116443",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/256KB",
+            "value": 34795730,
+            "range": "± 474939",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/256KB",
+            "value": 33451465,
+            "range": "± 535148",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/2MB",
+            "value": 304067753,
+            "range": "± 3320245",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/2MB",
+            "value": 262376531,
+            "range": "± 5638801",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/16MB",
+            "value": 2299741412,
+            "range": "± 37856557",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/16MB",
+            "value": 2167283821,
+            "range": "± 49562425",
             "unit": "ns/iter"
           }
         ]
